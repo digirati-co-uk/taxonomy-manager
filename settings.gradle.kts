7e6bece1541614fbe5d0bdb27582ff614a39b19c
Gary Tierney <gary.tierney@fastmail.com>
2019-06-11 16:44:23 +0100

    Add a web server skeleton to the Gradle project
    Re-organizes the Gradle project structure to move common configuration
    into a single `subprojects` block and moves the root project under a new
    `taxonomy-manager-engine` project. With these changes, taxman-engine has
    been changed from a "java" project to a "java-library" project. See:
    https://docs.gradle.org/current/userguide/java_library_plugin.html
    
    Additionally, the versions in `gradle.properties` have been inlined into
    the `dependencies { ... }` block to support scanning by dependabot.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
 rootProject.name = "digirati-taxonomy-manager"
+
+include("taxonomy-manager-engine")
+include("taxonomy-manager-rest-server")
+
+/* Update this when the Quarkus gradle plugin is available on the Gradle Plugin Portal. */
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "io.quarkus") {
+                useModule("io.quarkus:quarkus-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
