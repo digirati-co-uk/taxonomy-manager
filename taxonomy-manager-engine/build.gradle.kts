1b650429f58e84686cba7179dc48866269ec69a4
Gary Tierney <gary.tierney@fastmail.com>
2019-06-14 15:51:14 +0100

    Set up dependencies from rest-server to engine

--- taxonomy-manager-engine/build.gradle.kts
+++ taxonomy-manager-engine/build.gradle.kts
@@ -14,4 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.4.2")
     testImplementation("org.hamcrest", "hamcrest", "2.1")
     testImplementation("org.mockito", "mockito-junit-jupiter", "2.27.0")
+
+    api("org.apache.jena:jena-core:3.12.0")
+    api("org.apache.jena:apache-jena-libs:3.12.0")
 }
