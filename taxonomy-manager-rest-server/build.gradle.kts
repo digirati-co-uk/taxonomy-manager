a9387015fbd92543d35603af2487a6f7963e3dc4
Gary Tierney <gary.tierney@fastmail.com>
2019-06-14 16:30:56 +0100

    Add missing dependency on Google Guava

--- taxonomy-manager-rest-server/build.gradle.kts
+++ taxonomy-manager-rest-server/build.gradle.kts
@@ -19,11 +19,13 @@ val quarkusExtensions = setOf(
 
 dependencies {
     compile(enforcedPlatform("io.quarkus:quarkus-bom:0.16.1"))
-    compile(project(":taxonomy-manager-engine"))
 
     quarkusExtensions.forEach { ext ->
         compile("io.quarkus:quarkus-$ext:0.16.1")
     }
+
+    compile(project(":taxonomy-manager-engine"))
+    compile("com.google.guava", "guava", "27.1-jre")
 
     testCompile("io.quarkus", "quarkus-junit5", "0.16.1")
 }
