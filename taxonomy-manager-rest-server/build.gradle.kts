e3bdf42616d3fe84203bd5df3aa9fac8bd13700d
Gary Tierney <gary.tierney@fastmail.com>
2019-06-20 00:24:32 +0100

    Fix classpath issues with Quarkus devtools

--- taxonomy-manager-rest-server/build.gradle.kts
+++ taxonomy-manager-rest-server/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.quarkus.gradle.tasks.QuarkusDev
+
 plugins {
     java
     jacoco
@@ -13,22 +15,47 @@ repositories {
 val quarkusExtensions = setOf(
         "agroal",
         "flyway",
+        "hibernate-validator",
         "jdbc-postgresql",
         "resteasy",
         "resteasy-jsonb"
 )
 
+java {
+    sourceSets.getByName("main").output.setResourcesDir("build/classes/java/main")
+}
+
 dependencies {
-    compile(enforcedPlatform("io.quarkus:quarkus-bom:0.16.1"))
+    implementation(enforcedPlatform("io.quarkus:quarkus-bom:0.16.1"))
 
     quarkusExtensions.forEach { ext ->
-        compile("io.quarkus:quarkus-$ext:0.16.1")
+        implementation("io.quarkus:quarkus-$ext:0.16.1")
     }
+    implementation("org.springframework", "spring-jdbc", "5.1.3.RELEASE")
+    implementation("org.json", "json", "20180813")
 
-    compile(project(":taxonomy-manager-common"))
-    compile(project(":taxonomy-manager-engine"))
-    compile(project(":taxonomy-manager-rest"))
-    compile("com.google.guava", "guava", "27.1-jre")
+    implementation(project(":taxonomy-manager-common"))
+    implementation(project(":taxonomy-manager-engine"))
+    implementation(project(":taxonomy-manager-rest"))
+    implementation("com.google.guava", "guava", "27.1-jre")
 
-    testCompile("io.quarkus", "quarkus-junit5", "0.16.1")
+    testImplementation("io.quarkus", "quarkus-junit5", "0.16.1")
 }
+
+fun getOutputs(project: Project) = project.sourceSets.getByName("main").output
+
+// TODO: This is a temporary fix for Quarkus Devtools being unable to recognize
+// the classpath of projects in a multi-module Gradle build.
+tasks.register("mergeBuildOutput") {
+    doLast {
+        copy {
+            from(getOutputs(project(":taxonomy-manager-common")))
+            from(getOutputs(project(":taxonomy-manager-engine")))
+            from(getOutputs(project(":taxonomy-manager-rest")))
+
+            into("$buildDir/classes/java/main/")
+        }
+    }
+}
+
+tasks["classes"].dependsOn("mergeBuildOutput")
