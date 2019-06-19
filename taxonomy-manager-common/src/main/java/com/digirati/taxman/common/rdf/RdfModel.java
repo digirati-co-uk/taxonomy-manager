d1324c05371688382f26731419024276d99fc477
Gary Tierney <gary.tierney@fastmail.com>
2019-06-19 01:27:10 +0100

    Create a validator for SKOS concepts

--- taxonomy-manager-common/src/main/java/com/digirati/taxman/common/rdf/RdfModel.java
+++ taxonomy-manager-common/src/main/java/com/digirati/taxman/common/rdf/RdfModel.java
@@ -1,7 +1,29 @@
 package com.digirati.taxman.common.rdf;
 
+import com.google.common.collect.Streams;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 
 public interface RdfModel {
     Resource getResource();
+
+    default Map<String, String> getPlainLiteral(Property property) {
+        return Streams.stream(getResource().listProperties(property))
+            .collect(
+                Collectors.toMap(
+                    Statement::getLanguage, Statement::getString, (l, r) -> l));
+    }
+
+    default URI getUri() {
+        try {
+            return new URI(getResource().getURI());
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException("RdfModel has an invalid URI", ex);
+        }
+    }
 }
--- taxonomy-manager-common/src/main/java/com/digirati/taxman/common/taxonomy/ConceptRdfModel.java
+++ taxonomy-manager-common/src/main/java/com/digirati/taxman/common/taxonomy/ConceptRdfModel.java
@@ -4,19 +4,79 @@ import com.digirati.taxman.common.rdf.RdfModel;
 import com.digirati.taxman.common.rdf.annotation.RdfConstructor;
 import com.digirati.taxman.common.rdf.annotation.RdfType;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.SKOS;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RdfType("http://www.w3.org/2004/02/skos/core#Concept")
-public final class ConceptRdfModel implements RdfModel  {
+public final class ConceptRdfModel implements RdfModel {
 
     private final Resource resource;
+    private UUID uuid;
 
     @RdfConstructor
     public ConceptRdfModel(Resource resource) {
         this.resource = resource;
+        this.uuid = null;
+    }
+
+    public ConceptRdfModel(UUID uuid, Resource resource) {
+        this.uuid = uuid;
+        this.resource = resource;
+    }
+
+    public Optional<UUID> getUuid() {
+        return Optional.ofNullable(uuid);
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public boolean isNew() {
+        return uuid == null;
     }
 
     @Override
     public Resource getResource() {
         return resource;
+    }
+
+    public Map<String, String> getPreferredLabel() {
+        return getPlainLiteral(SKOS.prefLabel);
+    }
+
+    public Map<String, String> getAltLabel() {
+        return getPlainLiteral(SKOS.altLabel);
+    }
+
+    public Map<String, String> getHiddenLabel() {
+        return getPlainLiteral(SKOS.hiddenLabel);
+    }
+
+    public Map<String, String> getNote() {
+        return getPlainLiteral(SKOS.note);
+    }
+
+    public Map<String, String> getChangeNote() {
+        return getPlainLiteral(SKOS.changeNote);
+    }
+
+    public Map<String, String> getEditorialNote() {
+        return getPlainLiteral(SKOS.editorialNote);
+    }
+
+    public Map<String, String> getExample() {
+        return getPlainLiteral(SKOS.example);
+    }
+
+    public Map<String, String> getHistoryNote() {
+        return getPlainLiteral(SKOS.historyNote);
+    }
+
+    public Map<String, String> getScopeNote() {
+        return getPlainLiteral(SKOS.scopeNote);
     }
 }
