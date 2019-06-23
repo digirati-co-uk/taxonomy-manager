24ff380fc4849bfdc041e60ee1e4788a57453dd1
Gary Tierney <gary.tierney@fastmail.com>
2019-06-23 22:37:51 +0100

    Baseline and migrate database on startup
    Run migrations with Flyway at startup using a startup listener, allowing
    us to baseline the database in the scenario where no migrations have
    been run yet.

---
+++ taxonomy-manager-rest-server/src/main/java/com/digirati/taxman/rest/server/infrastructure/lifecycle/DatabaseInitListener.java
@@ -0,0 +1,20 @@
+package com.digirati.taxman.rest.server.infrastructure.lifecycle;
+
+import io.quarkus.runtime.StartupEvent;
+import org.flywaydb.core.Flyway;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.Properties;
+
+@ApplicationScoped
+public class DatabaseInitListener {
+
+    @Inject Flyway flyway;
+
+    void onStart(@Observes StartupEvent event) {
+        flyway.baseline();
+        flyway.migrate();
+    }
+}
