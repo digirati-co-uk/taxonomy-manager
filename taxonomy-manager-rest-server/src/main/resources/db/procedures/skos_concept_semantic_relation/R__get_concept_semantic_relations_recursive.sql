2668f63a8a42452f75754f5b246c8bd1773a6c91
Gary Tierney <gary.tierney@fastmail.com>
2019-07-09 10:14:36 +0100

    Fix recursive relationships procedure

--- taxonomy-manager-rest-server/src/main/resources/db/procedures/skos_concept_semantic_relation/R__get_concept_semantic_relations_recursive.sql
+++ taxonomy-manager-rest-server/src/main/resources/db/procedures/skos_concept_semantic_relation/R__get_concept_semantic_relations_recursive.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION get_concept_semantic_relations_recursive(source_uuid uuid, relation_type skos_semantic_relation_type)
+CREATE OR REPLACE FUNCTION get_concept_semantic_relations_recursive(_uuid uuid, _type skos_semantic_relation_type)
 RETURNS TABLE (
     source_uuid            uuid,
     target_uuid            uuid,
@@ -10,36 +10,45 @@ AS
 $$
 BEGIN
     RETURN QUERY
-        WITH RECURSIVE relationships (source_id, target_id, relation, transitive) AS (
-            -- First, select all the top-level relationships for our `source_id`.
-            SELECT sr.target_id,
-                   sr.relation,
-                   sr.transitive,
-                   1 as depth
-            FROM skos_concept_semantic_relation sr
-                INNER JOIN skos_concept sc
-                    ON sc.id = sr.source_id
-            WHERE sc.uuid = source_uuid
-              AND sr.relation = relation_type
-            UNION ALL
-            -- Then, iterate over the relationships of all `target_id`s.
-            SELECT sr.target_id,
-                   sr.relation,
-                   sr.transitive,
-                   rsr.depth + 1 as depth
-            FROM skos_concept_semantic_relation sr, relationships rsr
-            WHERE sr.source_id = rsr.target_id
-              AND sr.relation = relation_type
-        )
-        SELECT scs.uuid as source_uuid,
-               sct.uuid as target_uuid,
-               sct.preferred_label as target_preferred_label,
+        WITH RECURSIVE relationships (source_id, target_id, relation, transitive, depth, visited_ids, cycle) AS
+                           (
+                               -- First, select all the top-level relationships for our `source_id`.
+                               SELECT sr.source_id,
+                                      sr.target_id,
+                                      sr.relation,
+                                      sr.transitive,
+                                      1                    AS depth,
+                                      ARRAY [sr.source_id] AS visited_ids,
+                                      FALSE                AS is_cycle
+                               FROM skos_concept_semantic_relation sr
+                                        INNER JOIN skos_concept sc
+                                                   ON sc.id = sr.source_id
+                               WHERE sr.relation = _type
+                                 AND sc.uuid = _uuid
+                               UNION ALL
+                               -- Then, iterate over the relationships of all `target_id`s.
+                               SELECT sr.source_id,
+                                      sr.target_id,
+                                      sr.relation,
+                                      TRUE,
+                                      rsr.depth + 1 AS depth,
+                                      rsr.visited_ids || sr.source_id,
+                                      sr.source_id = ANY (rsr.visited_ids)
+                               FROM relationships rsr
+                                        INNER JOIN skos_concept_semantic_relation sr
+                                                   ON sr.relation = _type AND sr.source_id = rsr.target_id
+                               WHERE NOT cycle
+                                 AND rsr.depth < 5)
+        SELECT scs.uuid            AS source_uuid,
+               sct.uuid            AS target_uuid,
+               sct.preferred_label AS target_preferred_label,
+               r.relation,
                r.transitive,
-               r.relation
+               r.depth
         FROM relationships r
-            INNER JOIN skos_concept scs
-                ON scs.id = r.source_id
-            INNER JOIN skos_concept sct
-                ON sct.id = r.target_id;
+                 inner join skos_concept scs
+                            ON scs.id = r.source_id
+                 inner join skos_concept sct
+                            ON sct.id = r.target_id;
 END;
 $$ LANGUAGE plpgsql;
