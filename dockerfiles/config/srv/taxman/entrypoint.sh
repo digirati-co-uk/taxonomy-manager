983dbab8cba8881cf18bab00fb4a650254e38204
Daniel Grant <daniel.grant@digirati.com>
2019-07-11 11:40:49 +0100

    Updated Dockerfile.jvm to use entrypoint.sh to allow properties to be configured at runtime

---
+++ dockerfiles/config/srv/taxman/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+
+main() {
+    set_java_opts
+	run_backend "$@"
+}
+
+set_java_opts() {
+    JAVA_OPTS="$JAVA_OPTS \
+        -Dquarkus.datasource.url=${QUARKUS_DATASOURCE_URL} \
+        -Dquarkus.datasource.driver=${QUARKUS_DATASOURCE_DRIVER} \
+        -Dquarkus.datasource.username=${QUARKUS_DATASOURCE_USERNAME} \
+        -Dquarkus.datasource.password=${QUARKUS_DATASOURCE_PASSWORD} \
+        -Dmp.jwt.verify.publickey.location=${MP_JWT_VERIFY_PUBLICKEY_LOCATION} \
+        -Dmp.jwt.verify.issuer=${MP_JWT_VERIFY_ISSUER}"
+}
+
+run_backend() {
+	set -e
+	exec "$@"
+}
+
+main "$@"
