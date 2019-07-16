fe84b1e88b03f771a5a03c82a758208fd26821c0
Rob March <43951523+robmarch2@users.noreply.github.com>
2019-07-16 13:41:27 +0100

    Require authentication on project endpoints

--- taxonomy-manager-rest/src/main/java/com/digirati/taxman/rest/taxonomy/ProjectResource.java
+++ taxonomy-manager-rest/src/main/java/com/digirati/taxman/rest/taxonomy/ProjectResource.java
@@ -3,7 +3,9 @@ package com.digirati.taxman.rest.taxonomy;
 import com.digirati.taxman.common.rdf.annotation.jsonld.JsonLdFrame;
 import com.digirati.taxman.common.taxonomy.ProjectModel;
 import com.digirati.taxman.rest.MediaTypes;
+import com.digirati.taxman.rest.Roles;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
@@ -21,16 +23,19 @@ public interface ProjectResource {
     @Consumes({MediaTypes.APPLICATION_RDF_XML_VALUE, MediaTypes.APPLICATION_JSONLD_SKOS_VALUE})
     @Produces({MediaTypes.APPLICATION_RDF_XML_VALUE, MediaTypes.APPLICATION_JSONLD_SKOS_VALUE})
     @JsonLdFrame(input = "jsonld/framing/project.json")
+    @RolesAllowed(Roles.ADMIN)
     Response createProject(@Valid ProjectModel project);
 
     @GET
     @Path("/{project}")
     @Produces({MediaTypes.APPLICATION_JSONLD_SKOS_VALUE, MediaTypes.APPLICATION_RDF_XML_VALUE})
     @JsonLdFrame(input = "jsonld/framing/project.json")
+    @RolesAllowed(Roles.ADMIN)
     Response getProject(@BeanParam ProjectPath projectPath);
 
     @PUT
     @Path("/{project}")
     @Consumes({MediaTypes.APPLICATION_RDF_XML_VALUE, MediaTypes.APPLICATION_JSONLD_SKOS_VALUE})
+    @RolesAllowed(Roles.ADMIN)
     Response updateProject(@BeanParam ProjectPath projectPath, @Valid ProjectModel project);
 }
