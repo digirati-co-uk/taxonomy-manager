84d9f2b998c995965b0ee3a552b36729bde633f2
Gary Tierney <gary.tierney@fastmail.com>
2019-09-23 20:39:20 +0100

    Fix searching on multi-value literals

--- taxonomy-manager-rest-server/src/main/resources/db/procedures/skos_concept/R__get_concepts_by_partial_label.sql
+++ taxonomy-manager-rest-server/src/main/resources/db/procedures/skos_concept/R__get_concepts_by_partial_label.sql
@@ -15,8 +15,15 @@ DROP FUNCTION IF EXISTS contains_label_prefix;
 CREATE OR REPLACE FUNCTION contains_label_prefix(_json jsonb, _label text, _language text) RETURNS boolean AS
 $$
 BEGIN
-    RETURN EXISTS(SELECT 1
-                  FROM json_array_elements_text((_json -> _language)::json) value
-                  WHERE value::text ~ ('^' || _label));
+    IF jsonb_typeof(_json -> _language) = 'array' THEN
+        RETURN EXISTS(SELECT 1
+                      FROM json_array_elements_text((_json -> _language)::json) value
+                      WHERE value::text ~ ('^' || _label));
+    ELSE
+        RETURN EXISTS(SELECT 1
+                      FROM jsonb_each_text(_json) AS preferred_label
+                      WHERE preferred_label.key = _language
+                        AND preferred_label.value::text ~ ('^' || _label));
+    END IF;
 END
 $$ LANGUAGE plpgsql;
