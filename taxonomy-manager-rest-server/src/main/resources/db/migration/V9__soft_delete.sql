7b0bece21fe2d338bcfbf05d7e96ddad4645fb11
p-kaczynski <pawel.kaczynski@digirati.com>
2019-11-08 16:48:07 +0000

    Added the delete flag for projects, concepts and concept schemes.
    Added the indexes and views to take advantage of the flag.

---
+++ taxonomy-manager-rest-server/src/main/resources/db/migration/V9__soft_delete.sql
@@ -0,0 +1,68 @@
+-- CONCEPT
+
+alter table skos_concept
+    add deleted boolean default false not null;
+
+create index skos_concept_note_deleted
+    on skos_concept (deleted)
+    where deleted = false;
+
+create or replace view skos_concept_ex as
+    select * from skos_concept where deleted = false;
+
+create index skos_concept_uuid_key_not_deleted
+    on skos_concept (uuid)
+    where deleted = false;
+
+create index skos_concept__uniq_source_not_deleted
+    on skos_concept (source)
+    where deleted = false;
+
+create index skos_concept_pkey_not_deleted
+    on skos_concept (id)
+    where deleted = false;
+
+
+-- PROJECT
+
+alter table project
+    add deleted boolean default false not null;
+
+create index project_note_deleted
+    on project (deleted)
+    where deleted = false;
+
+create or replace view project_ex as
+    select * from project where deleted = false;
+
+create index project_slug_key_not_deleted
+    on project (slug)
+    where deleted = false;
+
+create index project_pkey_not_deleted
+    on project (id)
+    where deleted = false;
+
+-- CONCEPT SCHEME
+
+alter table skos_concept_scheme
+    add deleted boolean default false not null;
+
+create index skos_concept_scheme_note_deleted
+    on skos_concept_scheme (deleted)
+    where deleted = false;
+
+create or replace view skos_concept_scheme_ex as
+    select * from skos_concept_scheme where deleted = false;
+
+create index skos_concept_scheme_uuid_key_not_deleted
+    on skos_concept_scheme (uuid)
+    where deleted = false;
+
+create index skos_concept_scheme__uniq_source_not_deleted
+    on skos_concept_scheme (source)
+    where deleted = false;
+
+create index skos_concept_scheme_pkey_not_deleted
+    on skos_concept_scheme (id)
+    where deleted = false;
