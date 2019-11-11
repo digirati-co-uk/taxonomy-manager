ebd0f40dc306c6abb4ba5d40e54058e3e84de915
p-kaczynski <pawel.kaczynski@digirati.com>
2019-11-11 11:36:45 +0000

    Created delete procedures for concepts, concept schemes and projects.
    The procedures are trivial, as they set the delete flag to true based on id.

---
+++ taxonomy-manager-rest-server/src/main/resources/db/procedures/skos_concept/R__delete_concept.sql
@@ -0,0 +1,11 @@
+DROP PROCEDURE IF exists delete_concept;
+DROP FUNCTION IF EXISTS delete_concept;
+
+CREATE OR REPLACE PROCEDURE delete_concept(_uuid uuid)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    UPDATE skos_concept concept SET deleted = true WHERE concept.uuid = uuid;
+END;
+$$;
